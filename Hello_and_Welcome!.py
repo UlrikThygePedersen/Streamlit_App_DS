64bd48ae12904af7d6ce44fea7414d362980e0d7
Ulrik Pedersen <ulriktpedersen@gmail.com>
2023-03-24 12:36:13 +0100

    more pages

--- Hello_and_Welcome!.py
+++ Hello_and_Welcome!.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+# Page config
 st.set_page_config(
     page_title="Data Science Salary",
     page_icon="👨‍🔬",
@@ -15,11 +16,6 @@ st.markdown(
     ### Currently there are datasets and models from the following sources:
     - [Danish Data Science Association](https://ddsa.dk/)
     - [Reddit Data Science](https://www.reddit.com/r/datascience/comments/11xq4oo/data_scientist_salary_in_eu_2023_thread/?utm_source=share&utm_medium=ios_app&utm_name=iossmf&utm_content=1&utm_term=15)
-    - Ask a question in our [community
-        forums](https://discuss.streamlit.io)
-    ### See more complex demos
-    - Use a neural net to [analyze the Udacity Self-driving Car Image
-        Dataset](https://github.com/streamlit/demo-self-driving)
-    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+    - [Danish Industrial Association](https://www.danskindustri.dk/)
 """
 )
